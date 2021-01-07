f8e9b35846af7f63cd9e66c3859231cc2719e0ad
tkmn0 <tkmgin0914@gmail.com>
2021-01-08 06:32:59 +0900

    refactor: code format NoiseReducerControl.cs

--- Assets/Caress/_Examples/Scripts/NoiseReducerControl.cs
+++ Assets/Caress/_Examples/Scripts/NoiseReducerControl.cs
@@ -7,22 +7,19 @@ namespace Caress.Examples
 {
     public class NoiseReducerControl : MonoBehaviour
     {
-        [Header("UI")]
-        [SerializeField] private Button _playmodeButton = default;
+        [Header("UI")] [SerializeField] private Button _playmodeButton = default;
         [SerializeField] private Button _attenuationReduceButton = default;
         [SerializeField] private Button _attenuationAddButton = default;
         [SerializeField] private Button _noiseModelButton = default;
         [SerializeField] private Text _toggleButtonText = default;
         [SerializeField] private Text _attenuationValueText = default;
         [SerializeField] private Text _noiseModelText = default;
-        [Header("Value")]
-        [SerializeField] private MixerState _mixerState = MixerState.NoiseReduced;
+        [Header("Value")] [SerializeField] private MixerState _mixerState = MixerState.NoiseReduced;
         [SerializeField] private double _attenuation = 20;
         [SerializeField] private RnNoiseModel _noiseModel = RnNoiseModel.None;
-        [Header("Feature")]
-        [SerializeField] private AudioMixer _audioMixer = default;
+        [Header("Feature")] [SerializeField] private AudioMixer _audioMixer = default;
         [SerializeField] private NoiseReducerHandler _noiseReducerHandler = default;
-        
+
         private void Start()
         {
             //PlayMode
@@ -54,7 +51,7 @@ namespace Caress.Examples
 
         private void PlayModeButtonClicked()
         {
-            var next = _mixerState == MixerState.Original ?  MixerState.NoiseReduced : MixerState.Original;
+            var next = _mixerState == MixerState.Original ? MixerState.NoiseReduced : MixerState.Original;
             _audioMixer.SetFloat(_mixerState.ToString(), -80);
             _audioMixer.SetFloat(next.ToString(), 0);
             _mixerState = next;
@@ -77,7 +74,7 @@ namespace Caress.Examples
 
         private void NoiseModelButtonClicked()
         {
-            _noiseModel = _noiseModel == RnNoiseModel.None ? 0 : _noiseModel+1;
+            _noiseModel = _noiseModel == RnNoiseModel.None ? 0 : _noiseModel + 1;
             _noiseReducerHandler.NoiseReducer?.ChangeRnnModel(_noiseModel);
             _noiseModelText.text = _noiseModel.ToString();
         }
