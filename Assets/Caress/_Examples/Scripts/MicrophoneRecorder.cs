7cf22cd6bc0ce30cc1e0411bfe10305b64d6a9c9
tkmn0 <tkmgin0914@gmail.com>
2021-01-08 06:32:18 +0900

    refactor: code format MicrophoneRecorder.cs

--- Assets/Caress/_Examples/Scripts/MicrophoneRecorder.cs
+++ Assets/Caress/_Examples/Scripts/MicrophoneRecorder.cs
@@ -12,7 +12,7 @@ namespace Caress.Examples
         private int _clipHead;
         private readonly float[] _processBuffer = new float[480];
         private readonly float[] _microphoneBuffer = new float[RecordeLengthSec * SampleRate];
-        
+
         void Start()
         {
             _microphoneClip = Microphone.Start(null, true, RecordeLengthSec, SampleRate);
@@ -22,21 +22,23 @@ namespace Caress.Examples
         {
             var position = Microphone.GetPosition(null);
             if (position < 0 || _clipHead == position) return;
-            
+
             _microphoneClip.GetData(_microphoneBuffer, 0);
-            
-            int GetDataLength(int bufferLength, int head, int tail) => head < tail ? tail - head : bufferLength - head + tail;
+
+            int GetDataLength(int bufferLength, int head, int tail) =>
+                head < tail ? tail - head : bufferLength - head + tail;
+
             while (GetDataLength(_microphoneBuffer.Length, _clipHead, position) > _processBuffer.Length)
             {
                 var remain = _microphoneBuffer.Length - _clipHead;
                 if (remain < _processBuffer.Length)
                 {
                     Array.Copy(_microphoneBuffer, _clipHead, _processBuffer, 0, remain);
-                    Array.Copy(_microphoneBuffer,0,_processBuffer,0,_processBuffer.Length);
+                    Array.Copy(_microphoneBuffer, 0, _processBuffer, 0, _processBuffer.Length);
                 }
                 else
                 {
-                    Array.Copy(_microphoneBuffer, _clipHead, _processBuffer, 0 ,_processBuffer.Length);
+                    Array.Copy(_microphoneBuffer, _clipHead, _processBuffer, 0, _processBuffer.Length);
                 }
 
                 OnAudioReady?.Invoke(_processBuffer);
@@ -47,7 +49,5 @@ namespace Caress.Examples
                 }
             }
         }
-        
-   
     }
 }
