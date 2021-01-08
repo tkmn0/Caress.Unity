ee8f19d89d1c4d5a9eabdaad736553f0b1131b81
tkmn0 <tkmgin0914@gmail.com>
2021-01-08 09:26:29 +0900

    feat: add encoder functions to Encoder.cs

--- Assets/Caress/Encoder.cs
+++ Assets/Caress/Encoder.cs
@@ -79,5 +79,93 @@ namespace Caress
 
             return result.Value;
         }
+
+        public void SetComplexity(int complexity)
+        {
+            if (_ptr == IntPtr.Zero) return;
+            NativeMethods.EncoderSetComplexity(_ptr, complexity, out var error);
+            if (error.Code != (byte) ErrorCode.CaressOk)
+            {
+                throw new Exception(error.Data.StringValue());
+            }
+        }
+
+        public int GetComplexity()
+        {
+            if (_ptr == IntPtr.Zero) return 0;
+            NativeMethods.EncoderGetComplexity(_ptr, out var result);
+            if (result.Error.Code != (byte) ErrorCode.CaressOk)
+            {
+                throw new Exception(result.Error.Data.StringValue());
+            }
+
+            return result.Value;
+        }
+
+        public void SetSignal(EncoderSignal signal)
+        {
+            if (_ptr == IntPtr.Zero) return;
+            NativeMethods.EncoderSetSignal(_ptr, (int) signal, out var error);
+            if (error.Code != (byte) ErrorCode.CaressOk)
+            {
+                throw new Exception(error.Data.StringValue());
+            }
+        }
+
+        public EncoderSignal GetSignal()
+        {
+            if (_ptr == IntPtr.Zero) return EncoderSignal.None;
+            NativeMethods.EncoderGetSignal(_ptr, out var result);
+            if (result.Error.Code != (byte) ErrorCode.CaressOk)
+            {
+                throw new Exception(result.Error.Data.StringValue());
+            }
+
+            return (EncoderSignal) result.Value;
+        }
+
+        public void SetInBandFEC(bool enable)
+        {
+            if (_ptr == IntPtr.Zero) return;
+            NativeMethods.EncoderSetInBandFEC(_ptr, enable, out var error);
+            if (error.Code != (byte) ErrorCode.CaressOk)
+            {
+                throw new Exception(error.Data.StringValue());
+            }
+        }
+
+        public bool GetInBandFEC()
+        {
+            if (_ptr == IntPtr.Zero) return false;
+            NativeMethods.EncoderGetInBandFEC(_ptr, out var result);
+            if (result.Error.Code != (byte) ErrorCode.CaressOk)
+            {
+                throw new Exception(result.Error.Data.StringValue());
+            }
+
+            return result.Value;
+        }
+
+        public void SetPacketLossPercentage(int percentage)
+        {
+            if (_ptr == IntPtr.Zero) return;
+            NativeMethods.EncoderSetPacketLossPercentage(_ptr, percentage, out var error);
+            if (error.Code != (byte) ErrorCode.CaressOk)
+            {
+                throw new Exception(error.Data.StringValue());
+            }
+        }
+
+        public int GetPacketLossPercentage()
+        {
+            if (_ptr == IntPtr.Zero) return 0;
+            NativeMethods.EncoderGetPacketLossPercentage(_ptr, out var result);
+            if (result.Error.Code != (byte) ErrorCode.CaressOk)
+            {
+                throw new Exception(result.Error.Data.StringValue());
+            }
+
+            return result.Value;
+        }
     }
 }
